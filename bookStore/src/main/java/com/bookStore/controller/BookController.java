0395078522ab5eef5c1174d070c4507df4d84511
PriyanshuWin <priyanshukhuas29@gmail.com>
2024-06-07 16:51:08 +0530

    files added

---
+++ bookStore/src/main/java/com/bookStore/controller/BookController.java
@@ -0,0 +1,68 @@
+package com.bookStore.controller;
+
+import java.util.List;
+
+import org.aspectj.apache.bcel.classfile.Module.Require;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers.Default;
+import com.bookStore.model.Book;
+import com.bookStore.service.BookService;
+
+
+@RestController
+@RequestMapping("/api/book")
+public class BookController {
+	
+	@Autowired
+	private BookService bookService;
+	
+	// adding new book in database
+		@PostMapping("/add-book")
+		public ResponseEntity<Book> addBook(@RequestBody Book book) {
+			return bookService.addBook(book);
+		}
+		
+		// getting all books from database
+		// take params for pagination as page size and page number
+		
+		@GetMapping("/all-books")
+		public ResponseEntity<List<Book>> findAllBooks(
+		        @RequestParam(value = "pageSize", defaultValue = "5", required = false) int pageSize,
+		        @RequestParam(value = "pageNumber", defaultValue = "0", required = false) int pageNumber
+		) {
+		    return bookService.findAllBooks(pageNumber, pageSize);
+		}
+
+		
+		//getting book with specific bookId
+		@GetMapping("/{bookId}")
+		public ResponseEntity<Book> findBook(@PathVariable int bookId) {
+			return bookService.findBook(bookId);
+		}
+		
+		//updating record in database for specific bookId
+		@PutMapping("update/{bookId}")
+		public ResponseEntity<Book> updateBook(@PathVariable int bookId, @RequestBody Book book) {
+			return bookService.updateBook(bookId, book);
+			
+		}
+		
+		// delete by specific id
+		@DeleteMapping("delete/{bookId}")
+		public ResponseEntity<String> deleteBook(@PathVariable int bookId) {
+			return bookService.deleteBook(bookId);
+		}
+	
+}
---
+++ bookStore/src/main/java/com/bookStore/service/BookService.java
@@ -0,0 +1,85 @@
+package com.bookStore.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.bookStore.model.Book;
+import com.bookStore.repository.BookRepository;
+
+@Service
+public class BookService {
+	
+	@Autowired
+	BookRepository bookRepository;
+
+	public ResponseEntity<Book> addBook(Book book) {
+		try {
+            Book savedBook = bookRepository.save(book);
+            return new ResponseEntity<>(savedBook, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+	}
+	
+
+	public ResponseEntity<List<Book>> findAllBooks(int pageNumber, int pageSize) {
+		// pagination
+		Pageable pageable = PageRequest.of(pageNumber, pageSize);
+		
+		Page<Book> booksOfPage = bookRepository.findAll(pageable);
+		List<Book> books = booksOfPage.getContent();
+		
+		return new ResponseEntity<>(books, HttpStatus.OK) ;
+	}
+	
+
+	public ResponseEntity<Book> findBook(int bookId) {
+		Optional<Book> book = bookRepository.findById(bookId);
+		if(book.isEmpty()) {
+			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(book.get(), HttpStatus.OK);
+	}
+	
+
+	public ResponseEntity<Book> updateBook(int bookId, Book book) {
+		Optional<Book> existingOptional = bookRepository.findById(bookId);
+		
+		if(existingOptional.isEmpty()) {
+			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		}
+		
+		Book existingBook = existingOptional.get();
+		existingBook.setTitle(book.getTitle());
+		existingBook.setDescription(book.getDescription());
+		existingBook.setAuthor(book.getAuthor());
+		existingBook.setPageCount(book.getPageCount());
+		
+		Book updatedBook = bookRepository.save(existingBook);
+		
+		return new ResponseEntity<>(updatedBook, HttpStatus.OK);
+	}
+
+	public ResponseEntity<String> deleteBook(int bookId) {
+		Optional<Book> book = bookRepository.findById(bookId);
+		
+		if(book.isEmpty()) {
+			return new ResponseEntity<>("Book not found", HttpStatus.NOT_FOUND);
+		}
+		
+		bookRepository.deleteById(bookId);
+		
+		return new ResponseEntity<>("Deleted succesfully", HttpStatus.OK);
+	}
+
+
+}
+
